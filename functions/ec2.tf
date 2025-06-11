c0d1c5b71a8947328c374303d3950b01c993054e
Suraj Padhi <surajpadhi4546@gmail.com>
2025-06-11 20:35:11 +0530

    Terraform practiced

--- functions/ec2.tf
+++ functions/ec2.tf
@@ -1,12 +1,17 @@
 resource "aws_instance" "roboshop" {
-  count = 4
+  count = length(var.instances)
   ami           = var.ami_id
-  instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
+  instance_type = each.value
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
 
-  tags = {
-    Name = var.instances[count.index]
-  
+  tags = merge(
+    var.common_tags,
+    {
+      Component = var.instances[count.index]
+      Name = var.instances[count.index]
+    }
+   
+    )
 }
 
 resource "aws_security_group" "allow_all" {
@@ -29,5 +34,10 @@ resource "aws_security_group" "allow_all" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = var.sg_tags
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "allow-all"
+    }
+  )
 }
