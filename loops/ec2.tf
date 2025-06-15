867bc6e3d1c9db6c1de3634a20cf683fd366c02e
Suraj Padhi <surajpadhi4546@gmail.com>
2025-06-15 20:11:17 +0530

    changed

--- loops/ec2.tf
+++ loops/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "roboshop" {
-  count = 4
-  ami           = var.ami_id
-  instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
-  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  count                  = 4
+  ami                    = var.ami_id
+  instance_type          = var.environment == "dev" ? "t3.micro" : "t3.small"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
     Name = var.instances[count.index]
-  
+  }
 }
 
 resource "aws_security_group" "allow_all" {
--- loops/variables.tf
+++ loops/variables.tf
@@ -1,64 +1,64 @@
 variable "ami_id" {
-    type = string
-    default = "ami-09c813fb71547fc4f"
-    description = "AMI ID of devops-practice RHEL9"
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "AMI ID of devops-practice RHEL9"
 }
 
 variable "instance_type" {
-    default = "t3.micro"  
+  default = "t3.micro"
 }
 
 variable "ec2_tags" {
-    type = map(string)
-    default = {
-        Name = "ec2-using-terraform"
-        Purpose = "Variables-demo"
-    }
+  type = map(string)
+  default = {
+    Name    = "ec2-using-terraform"
+    Purpose = "Variables-demo"
+  }
 }
 
 variable "sg_name" {
-    default = "allow-all"
-  
+  default = "allow-all"
+
 }
 
 variable "sg_description" {
-    default = "allowing all ports from internet"  
+  default = "allowing all ports from internet"
 }
 
 variable "from_port" {
-    default = 0
+  default = 0
 }
 
 variable "to_port" {
-    type = number
-    default = 0 
+  type    = number
+  default = 0
 }
 
 variable "cidr_blocks" {
-    type = list(string)
-    default = [ "0.0.0.0/0" ]  
+  type    = list(string)
+  default = ["0.0.0.0/0"]
 }
 
 variable "sg_tags" {
-    default = {
-        Name = "allow-all"
-    } 
+  default = {
+    Name = "allow-all"
+  }
 }
 
 variable "environment" {
-    default = "prod"  
+  default = "prod"
 }
 
 variable "instances" {
-    default = ["mongodb", "redis", "frontend", "Rabbitmq"]
-  
+  default = ["mongodb", "redis", "frontend", "Rabbitmq"]
+
 }
 
 variable "zone_id" {
-    default = "Z07092431FXHTW58PX8NM"
-  
+  default = "Z07092431FXHTW58PX8NM"
+
 }
 
 variable "domain_name" {
-    default = "skptech.site"  
+  default = "skptech.site"
 }
